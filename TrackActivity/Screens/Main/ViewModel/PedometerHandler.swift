175d8a467f9a31f81145ec1c7579a1c4c1e22467
milton <miltonpalaguachi@gmail.com>
2021-02-21 18:23:52 -0500

    moved Pedometer from View Controller to PedometerHandler class

--- TrackActivity/Screens/Main/ViewModel/PedometerHandler.swift
+++ TrackActivity/Screens/Main/ViewModel/PedometerHandler.swift
@@ -1,22 +1,22 @@
 //
-//  PedometorManager.swift
+//  PedometerHandler.swift
 //  TrackActivity
 //
 //  Created by Milton Palaguachi on 2/20/21.
 //
 
-import Foundation
+import UIKit
 import CoreMotion
 
-protocol PedometorManagerDelegate: AnyObject {
-    var paceLabel:String? {get}
-    var stepsLabel:String? {get}
-    var distanceLabel:String? {get}
-    var statusLable:String? {get}
+protocol PedometorHandlerDelegate: AnyObject {
+    func paceLabel(_ text: String)
+    func stepsLabel(_ text: String)
+    func distanceLabel(_ text: String)
+    func statusLabel(_ text: String)
 }
 
-class PedometorManager {
-    weak var delegate: PedometorManagerDelegate?
+class PedometerHandler {
+    
     private var pedometerData = CMPedometerData()
     private var pedometer = CMPedometer()
     
@@ -25,10 +25,28 @@ class PedometorManager {
     private var distance: Double = 0.0
     private var pace: Double = 0.0
     
+    public weak var delegate: PedometorHandlerDelegate?
+
     //For Calculating Pace
     private var elepsedSeconds: Double = 0.0
     private var interval: Double = 0.1
+    
     // MARK: Private Methods
+    func startStopPedometer(_ sender: UIButton) {
+        if sender.titleLabel?.text == PedometerStatus.start {
+            self.delegate?.statusLabel(PedometerStatus.pedemeterOn)
+            sender.setTitle(PedometerStatus.stop, for: .normal)
+            sender.backgroundColor = PedometerStatusColor.stopRed
+            self.stepCountingIfAvailabel()
+            
+        } else {
+            self.pedometer.stopUpdates()
+            self.stopTimer()
+            self.delegate?.statusLabel(PedometerStatus.pedemeterOff)
+            sender.backgroundColor = PedometerStatusColor.goGreen
+            sender.setTitle(PedometerStatus.start, for: .normal)
+        }
+    }
     
     //MARK: - Check If SteipCounting is Available
     private func stepCountingIfAvailabel() {
@@ -46,11 +64,11 @@ class PedometorManager {
     
     // MARK: Start Timer
     private func startTimer() {
-//        print("Start Timer \(Date())")
+        print("Start Timer \(Date())")
         guard !self.timer.isValid else { return }
         //Intervar: 1 second
         self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [unowned self] _ in
-//            self.displayPedometerData()
+            self.displayPedometerData()
             self.elepsedSeconds += self.interval
         })
     }
@@ -59,23 +77,22 @@ class PedometorManager {
     private func stopTimer() {
 //        print("Stop Timer \(Date())")
         self.timer.invalidate()
-//        self.displayPedometerData()
+        self.displayPedometerData()
     }
     
     // MARK: Display Pedometer Data
-    /*
+    
     func displayPedometerData() {
-        self.statusLabel.text = PedometerStatus.pedemeterOn + self.minutesSeconds(elepsedSeconds)
+        self.delegate?.statusLabel( PedometerStatus.pedemeterOn + self.minutesSeconds(elepsedSeconds))
         //Steps
         guard let numberOfSteps = self.numberOfSteps else { return }
-        self.stepsLabel.text = String(format: "Steps: %i", numberOfSteps)
-        print("\(Date()) --- \(self.stepsLabel.text ?? "")")
+        self.delegate?.stepsLabel(String(format: "Steps: %i", numberOfSteps))
         //Distance
         guard let pedometerDistance = self.pedometerData.distance, let distance = pedometerDistance as? Double else {
             return print("Unable to to get pedometer distance")
         }
         self.distance = distance
-        self.distanceLabel.text = String(format: "Distance %.2f  meters", self.distance)
+        self.delegate?.distanceLabel(String(format: "Distance %.2f  meters", self.distance))
         //Pace
         self.updatePace()
     }
@@ -85,17 +102,17 @@ class PedometorManager {
     private func updatePace() {
         if CMPedometer.isPaceAvailable() {
             guard let pedometerPace = self.pedometerData.averageActivePace, let pace = pedometerPace as? Double else {
-                self.paceLabel.text = "Pace: N/A"
+                self.delegate?.paceLabel("Pace: N/A")
                 return
             }
             self.pace = pace
-            self.paceLabel.text = String(format: "Avg Pace: %.2f min/mi", self.pace)
+            self.delegate?.paceLabel(String(format: "Avg Pace: %.2f min/mi", self.pace))
             
         } else {
-            self.paceLabel.text = String(format: "Avg Pace: %.2f min/mi", self.calculatedPace())
+            self.delegate?.paceLabel(String(format: "Avg Pace: %.2f min/mi", self.calculatedPace()))
         }
     }
-    */
+    
     func minutesSeconds(_ seconds: Double) -> String {
         let minute = Int(seconds)/60
         let secondsPart = Int(seconds) % 60
